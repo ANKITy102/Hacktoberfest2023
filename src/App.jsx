c163fe301ba3eff9e5fa066327c95f19a0bb9cc0
arya_n <aryan.arya@iitg.ac.in>
2023-10-02 16:04:03 +0530

    Added a Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+// import React from "react"
 import GlassNavbar from "./Components/Navbars/GlassNavbar/GlassNavbar"
+import Footer from "./Components/footer/Footer"
 function App() {
 
   return (
     <>
       <GlassNavbar/>
+      <Footer />
     </>
   )
 }
