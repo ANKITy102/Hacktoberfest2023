c66acf71ad524f35ae10c3d9bfd07fe139de2660
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-10-03 11:21:13 +0530

    Footer removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // import React from "react"
 import GlassNavbar from "./Components/Navbars/GlassNavbar/GlassNavbar"
-import Footer from "./Components/footer/footer"
 import Sidebar from "./Components/Sidebar"
 
 
@@ -10,7 +9,6 @@ function App() {
     <>
       <GlassNavbar/>
       <Sidebar />
-      <Footer />
 
     </>
   )
